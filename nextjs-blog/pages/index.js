cd02e832dd47406e1ba46e994ae03853a5f1aa01
Joel Frase <37312935+jfrase8@users.noreply.github.com>
2024-09-12 20:24:08 -0500

    did some tutorial stuff as well as messing around with some things

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <div className={utilStyles.gameTitle}>
+        Sound Showdown
+      </div>
+    </Layout>
+  );
+}
